d5fa22e0ff6d8f1e96f0c5441f5329f3a320327a
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-08-14 22:11:59 +0700

    Setup CORS
    Enable CORS for React.js server

--- backend-django/src/satudata/settings.py
+++ backend-django/src/satudata/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     # Imported Apps
     'rest_framework',
     'rest_framework.authtoken',
+    "corsheaders",
 
     # Created Apps
     'api',
@@ -57,11 +58,16 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'satudata.urls'
