723b9796fcc39372acb2dbad237cfce85bb10e62
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-08-30 15:16:06 +0700

    Update Tags
    Menambahkan fitur tag pada katalog

--- operation/admin.py
+++ operation/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Catalog
+from .models import Catalog, Tag
 
-# admin.site.register(Catalog)
+admin.site.register(Tag)
+
 @admin.register(Catalog)
 class CatalogAdmin(admin.ModelAdmin):
     list_display = ('title', 'published', 'author')
--- operation/models.py
+++ operation/models.py
@@ -1,16 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Catalog(models.Model):
     title = models.CharField(max_length=200, blank=False)
+    tags = models.ManyToManyField(Tag)
     url = models.URLField(max_length=200, unique=True, blank=False)
     description = models.TextField(blank=False)
     slug = models.SlugField(max_length=100, unique=True)
     published = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    # group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
