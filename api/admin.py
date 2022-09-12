726aef9d63f17a841c1ae0a9e5f76db34233e847
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-09-12 11:01:33 +0700

    Add table petani
    Menambahkan tabel petani

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Petani
 
 @admin.register(Article)
 class ArticleModel(admin.ModelAdmin):
     list_filter = ('title', 'author')
     list_display = ('title', 'author', 'description')
+
+@admin.register(Petani)
+class PetaniModel(admin.ModelAdmin):
+    list_filter = ('nik', 'gelar_depan', 'nama_lengkap', 'gelar_belakang', 'jenis_kelamin', 'desa', 'kecamatan', 'kabupaten')
+    list_display = ('nik', 'nama_lengkap', 'jenis_kelamin', 'id')
--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
+from turtle import title
 from django.db import models
+import uuid
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -7,3 +9,24 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Petani(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    nik = models.CharField(max_length=16, unique=True)
+    gelar_depan = models.CharField(max_length=50, blank=True)
+    nama_lengkap = models.CharField(max_length=100, blank=True)
+    gelar_belakang = models.CharField(max_length=50, blank=True)
+    jenis_kelamin = models.CharField(max_length=10, choices=[('Laki-laki', 'Laki-laki'), ('Perempuan', 'Perempuan')], blank=True)
+    desa = models.CharField(max_length=50, blank=True)
+    kecamatan = models.CharField(max_length=50, blank=True)
+    kabupaten = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.nama_lengkap
+    
+    def save(self, *args, **kwargs):
+        self.nama_lengkap = self.nama_lengkap.upper()
+        self.desa = self.desa.upper()
+        self.kecamatan = self.kecamatan.upper()
+        self.kabupaten = self.kabupaten.upper()
+        return super(Petani, self).save(*args, **kwargs)
