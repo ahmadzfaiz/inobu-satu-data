70a0c5c449d0a2131bbd46b573968437240a9da5
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-09-13 15:27:15 +0700

    Update table

--- api/models.py
+++ api/models.py
@@ -57,7 +57,7 @@ class Kegiatan(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     nama_kegiatan = models.CharField(max_length=100)
     tanggal_mulai_kegiatan = models.DateField()
-    tanggal_selesai_kegiatan = models.DateField()
+    tanggal_selesai_kegiatan = models.DateField(default='1/1/1900')
     sumber_pendanaan = models.ForeignKey('Sumber_Pendanaan',on_delete=models.CASCADE, blank=True, null=True)
     
     def __str__(self):
