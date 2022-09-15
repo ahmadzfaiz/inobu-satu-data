26d3a1235bd4239e3c29853c9ab00cbc1d2416d5
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-09-15 14:27:15 +0700

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -17,10 +17,13 @@ admin.site.register(Sumber_Pendanaan)
 
 @admin.register(Kegiatan)
 class KegiatanModel(admin.ModelAdmin):
+    search_fields = ('nama_kegiatan',)
     list_display = ('id', 'nama_kegiatan', 'tanggal_mulai_kegiatan', 'tanggal_selesai_kegiatan', 'sumber_pendanaan')
+    list_filter = ('nama_kegiatan', 'tanggal_mulai_kegiatan', 'tanggal_selesai_kegiatan', 'sumber_pendanaan')
     raw_id_fields = ('sumber_pendanaan',)
 
 @admin.register(Kegiatan_Petani)
 class KegiatanPetaniModel(admin.ModelAdmin):
     list_display = ('id', 'id_kegiatan', 'id_petani')
+    list_filter = ('id_kegiatan', 'id_petani')
     raw_id_fields = ('id_kegiatan', 'id_petani')
